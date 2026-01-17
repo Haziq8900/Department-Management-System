708f906056c5cc20a76c1e01dba8e0ea67765cca
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-01-17 12:26:09 +0500

    registration working successfull

--- src/main/java/com/departmentmanagementsystem/servlet/UserServlet.java
+++ src/main/java/com/departmentmanagementsystem/servlet/UserServlet.java
@@ -69,8 +69,10 @@ public class UserServlet extends HttpServlet {
                 String username = req.getParameter("username");
                 String password = req.getParameter("password");
                 String email = req.getParameter("email");
+                String role = req.getParameter("role");
+                User user = new User(username, password, email, role);
 
-                if (dao.registerUser(username, password, email)) {
+                if (dao.registerUser(user)) {
                     resp.sendRedirect(req.getContextPath() + "/login.jsp?message=registration_successful");
                 } else {
                     resp.sendRedirect(req.getContextPath() + "/register.jsp?error=username_taken");
